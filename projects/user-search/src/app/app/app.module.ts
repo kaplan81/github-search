c267295eede0e86a164769d55fe9d44ef9585c02
Andres Gesteira <gesteira.pro@gmail.com>
2019-05-16 01:39:06 +0200

    search form in header

--- projects/user-search/src/app/app/app.module.ts
+++ projects/user-search/src/app/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 // import * as fromRootComponents from '@user-search-app/components';
 import * as fromRootContainers from '@user-search-app/containers';
 import { CoreModule } from '@user-search-core/core.module';
+import { SharedModule } from '@user-search-shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [...fromRootContainers.containers],
-  imports: [BrowserModule, HttpClientModule, CoreModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, CoreModule, SharedModule, AppRoutingModule],
   bootstrap: [fromRootContainers.AppComponent]
 })
 export class AppModule {}
