cb198730799707c79c77c831161fdde40c646f54
Andres Gesteira <gesteira.pro@gmail.com>
2019-05-15 02:58:12 +0200

    user service

---
+++ projects/user-search/src/app/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ErrorService } from '@github-search/ng-kit';
+import { ConfigService } from '@user-search-core/config.service';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  static readonly userApiQuery = 'q';
+  static readonly userApiSort = 'sort';
+  static readonly userApiOrder = 'order';
+  private userApi: string;
+
+  constructor(
+    private configService: ConfigService,
+    private errorService: ErrorService,
+    private http: HttpClient
+  ) {
+    this.userApi = `${this.configService.apiBase}/${this.configService.apiPathEndpoints.users}`;
+  }
+
+  getUsers(search: string): Observable<any> {
+    const url = `${this.userApi}?${UserService.userApiQuery}=${search}`;
+
+    return this.http
+      .get<any>(url)
+      .pipe(catchError(this.errorService.handleHttpError<any>('getUsers')));
+  }
+}
