b6165de0a530a1839c6ad023671a130e6c38377c
Andres Gesteira <gesteira.pro@gmail.com>
2019-05-16 02:13:34 +0200

    display search form in header dynamically

--- projects/user-search/src/app/app/containers/app/app.component.ts
+++ projects/user-search/src/app/app/containers/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { RouterEventsService, SubscribedContainerWithRouter } from '@github-search/ng-kit';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -6,4 +8,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent extends SubscribedContainerWithRouter {
+  section: string;
+
+  constructor(public router: Router, private routerEventsService: RouterEventsService) {
+    super(router);
+    this.routerEventsService
+      .filterRouterEvents(this, NavigationEnd)
+      .subscribe((navigationEnd: any) => {
+        this.section = (navigationEnd as NavigationEnd).urlAfterRedirects;
+      });
+  }
+
+  hasSearchSection(): boolean {
+    return this.section === undefined || this.section === '/search';
+  }
+}
